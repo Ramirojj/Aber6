57722788d6aa860173d978b65b973fdf96deeb07
Ramirojj <126816654+Ramirojj@users.noreply.github.com>
2024-10-21 17:15:47 -0500

    [2.0] Create class dk and sf2

---
+++ sf2.cs
@@ -0,0 +1,8 @@
+public class sf2 : Character
+{
+  public List<string> Moves { get; set; } = [];
+  public override string Display()
+  {
+    return $"Id: {Id}\nName: {Name}\nDescription: {Description}\nMoves: {string.Join(", ", Moves)}\n";
+  }
+}
