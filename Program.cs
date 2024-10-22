53f1c536c7b91e144f0f5e374e7b617df9a8915a
Ramirojj <126816654+Ramirojj@users.noreply.github.com>
2024-10-22 00:02:56 -0500

    [2.01] Deserialize dong king and street fighters

--- Program.cs
+++ Program.cs
@@ -8,13 +8,25 @@ var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassL
 logger.Info("Program started");
 
 string marioFileName = "mario.json";
+string dkFileName = "dk.json";
+string sf2FileName = "sf2.json";
+
 List<Mario> marios = JsonSerializer.Deserialize<List<Mario>>(File.ReadAllText(marioFileName))!;
+List<dk> dks = JsonSerializer.Deserialize<List<dk>>(File.ReadAllText(dkFileName))!;
+List<sf2> sf2s = JsonSerializer.Deserialize<List<sf2>>(File.ReadAllText(sf2FileName))!;
+
 do
 {
   // display choices to user
   Console.WriteLine("1) Display Mario Characters");
   Console.WriteLine("2) Add Mario Character");
-  Console.WriteLine("3) Remove Mario Character");
+  Console.WriteLine("3) Remove Mario Character");/*
+  Console.WriteLine("4) Display Mario Donkey Kong Characters");
+  Console.WriteLine("5) Add donkey kong Character");
+  Console.WriteLine("6) Remove donkey kong Character");
+  Console.WriteLine("7) Display street fighter 2 Characters");
+  Console.WriteLine("8) Add street fighter 2 Character");
+  Console.WriteLine("9) Remove street fighter 2 Character");*/
   Console.WriteLine("Enter to quit");
   // input selection
   string? choice = Console.ReadLine();
--- dk.cs
+++ dk.cs
@@ -1,8 +1,17 @@
-public class dk : Character
+/*public class dk : Character
 {
   public List<string> Species { get; set; } = [];
   public override string Display()
   {
     return $"Id: {Id}\nName: {Name}\nDescription: {Description}\nSpecies: {string.Join(", ", Species)}\n";
   }
+}*/
+public class dk : Character
+{
+    public string? Species { get; set; }
+    
+    public override string Display()
+    {
+        return $"Id: {Id}\nName: {Name}\nDescription: {Description}\nSpecies: {Species}\n";
+    }
 }
